96d2d5fcdf49553e41403c87f5a6fe9e5f3cbd57
AzusaOu <happy_panda@126.com>
2025-04-30 00:36:58 +0800

    commit-20250430

--- azstd_py/file/rw.py
+++ azstd_py/file/rw.py
@@ -1,16 +1,19 @@
-## Last update: 2023.02.04
+## Last update: 2025.04.30
 
 def gracefulRead(fname, method='r'):
 	with open(fname, method) as o:
 		buf = o.read()
 	return buf
 
-def gracefulWrite(fname, buff, method='w'):
+def gracefulWrite(fname, buff, method='w', encoding='utf-8'):
 	try:
-		with open(fname, method) as o:
-			o.write(buff)
+		if method == 'w':
+			o = open(fname, method, encoding=encoding)
+		elif method == 'wb':
+			o = open(fname, method)
+		o.write(buff)
+		o.close()
 		return 0
-	except:
+	except Exception as e:
+		print(e)
 		return -1
-
-
--- azstd_py/out.py
+++ azstd_py/out.py
@@ -1,5 +1,8 @@
 import time
 
+def localtime(style='%Y-%m-%d %H:%M:%S'):
+	return time.strftime(style, time.localtime())
+
 class Log:
 	def __init__(self, show_level=1):
 		self.show_level = show_level
@@ -8,7 +11,7 @@ class Log:
 	def print(self, msg, level=1, write=False):
 		if self.show_level > level:
 			return None
-		localtime = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()) 
+		localtime = localtime()
 		msg = '[%s][%s] %s' % (localtime, self.level_map[level], msg)
 		print(msg)
 		if write == True:
