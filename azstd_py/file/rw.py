f2280376eca2fcb94317b8276333735404a09ecf
Nanami <happy_panda@126.com>
2025-05-20 23:25:35 +0900

    Update rw

--- azstd_py/file/rw.py
+++ azstd_py/file/rw.py
@@ -17,3 +17,11 @@ def gracefulWrite(fname, buff, method='w', encoding='utf-8'):
 	except Exception as e:
 		print(e)
 		return -1
+
+def readstream(fname, n_byte=1024):
+	with open(fname, 'rb') as o:
+		while True:
+			data = o.read(n_byte)
+			if not data:
+				return None
+			yield data
