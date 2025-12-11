a10e6434ca5b82aa5fb864193dd50a731ddeb418
CH3COOOH <happy_panda@126.com>
2025-12-12 00:23:42 +0900

    bug fixed.

--- azstd_py/out.py
+++ azstd_py/out.py
@@ -11,7 +11,7 @@ class Log:
 	def print(self, msg, level=1, write=False):
 		if self.show_level > level:
 			return None
-		localtime = localtime()
+		localtime = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
 		msg = '[%s][%s] %s' % (localtime, self.level_map[level], msg)
 		print(msg)
 		if write == True:
