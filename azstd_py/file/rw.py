10c124804b2266ab2de4a2587c875d65f7366e8f
hotaru <ch3coooh@henchat.net>
2026-03-08 14:58:05 +0900

    commit-20260308

--- azstd_py/file/rw.py
+++ azstd_py/file/rw.py
@@ -1,32 +1,32 @@
-## Last update: 2025.04.30
+## Last update: 2026.03.08
 
-def read_t8(fname):
+def read_t(fname: str):
+	with open(fname, 'r') as o:
+		buf = o.read()
+	return buf
+
+def read_t8(fname: str):
 	with open(fname, 'rb') as o:
 		buf = o.read().decode('utf-8')
 	return buf
 
-def gracefulRead(fname, method='r'):
-	with open(fname, method) as o:
-		buf = o.read()
+def write_t(fname: str, buf: str):
+	with open(fname, 'w') as o:
+		o.write(buf)
 	return buf
 
-def gracefulWrite(fname, buff, method='w', encoding='utf-8'):
-	try:
-		if method == 'w':
-			o = open(fname, method, encoding=encoding)
-		elif method == 'wb':
-			o = open(fname, method)
-		o.write(buff)
-		o.close()
-		return 0
-	except Exception as e:
-		print(e)
-		return -1
+def write_t8(fname: str, buf: str):
+	with open(fname, 'wb') as o:
+		o.write(buf.encode('utf-8'))
+	return buf
 
-def readstream(fname, n_byte=1024):
+def read_stream(fname, n_byte=1024):
 	with open(fname, 'rb') as o:
 		while True:
 			data = o.read(n_byte)
 			if not data:
 				return None
 			yield data
+
+if __name__ == '__main__':
+	write_t8('wt8.txt', '今が最高！')
--- azstd_py/out.py
+++ azstd_py/out.py
@@ -1,19 +1,33 @@
 import time
 
-def localtime(style='%Y-%m-%d %H:%M:%S'):
-	return time.strftime(style, time.localtime())
-
 class Log:
-	def __init__(self, show_level=1):
+	def __init__(self, show_level=1, logfile=None):
 		self.show_level = show_level
 		self.level_map = ['DEBUG', 'INFO', 'WARN', 'ERROR']
+		self.logfile = logfile
 
-	def print(self, msg, level=1, write=False):
+	def std_localtime(self):
+		return time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
+
+	def print(self, msg, level=1, write=False) -> int:
 		if self.show_level > level:
-			return None
-		localtime = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
+			return 1
+		localtime = self.std_localtime()
 		msg = '[%s][%s] %s' % (localtime, self.level_map[level], msg)
 		print(msg)
 		if write == True:
-			with open('event.log', 'a') as o:
-				o.write(msg + '\n')
+			if self.logfile == None:
+				print('** Logfile is not set. Ignore.')
+				return 1
+			try:
+				with open(self.logfile, 'a', encoding='utf-8') as o:
+					o.write(msg + '\n')
+			except:
+				print('** Unable to write out logfile. Ignore.')
+				return 1
+		return 0
+
+if __name__ == '__main__':
+	log = Log()
+	print(log.std_localtime())
+	log.print('This is a test log.')
